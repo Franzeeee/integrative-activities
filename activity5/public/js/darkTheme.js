820c2878d54c6c89364740172515cd44aeffbd53
Franze <diazfranzpeter@gmail.com>
2023-11-09 23:48:54 +0800

    Added Dark Theme

--- activity5/public/js/darkTheme.js
+++ activity5/public/js/darkTheme.js
@@ -1,12 +1,15 @@
 const themeSwitch = document.querySelector("#themeSwitch");
 const container = document.querySelector(".container");
+const nav = document.querySelector("nav");
 
 // Check if the user's theme preference is stored in localStorage
 const userThemePreference = localStorage.getItem('themePreference');
 
+// Apply the user's preferred theme (if stored)
 if (userThemePreference === 'lightTheme') {
     container.classList.add('lightTheme');
     themeSwitch.classList.add('themeSwitchChanged');
+    nav.classList.add('nav-light');
 }
 
 themeSwitch.addEventListener('click', function () {
@@ -14,6 +17,7 @@ themeSwitch.addEventListener('click', function () {
 
     container.classList.toggle("lightTheme");
     themeSwitch.classList.toggle("themeSwitchChanged");
+    nav.classList.toggle("nav-light");
 
     // Store the user's theme preference in localStorage
     const themePreference = container.classList.contains("lightTheme") ? 'lightTheme' : 'darkTheme';
