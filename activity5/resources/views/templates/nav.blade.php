bc9fcefd12ce35403702201eb2e53e8f8f9dae08
Franze <diazfranzpeter@gmail.com>
2023-11-10 22:38:44 +0800

    Updated Routes

--- activity5/resources/views/templates/nav.blade.php
+++ activity5/resources/views/templates/nav.blade.php
@@ -1,7 +1,7 @@
 <nav>
     <h1>Franz</h1>
     <ul>
-        <li><a href="{{route('goTo.Dashboard')}}">Dashboard</a></li>
-        <li><a href="{{route('goTo.Register')}}">Register</a></li>
+        <li><a href="{{route('dashboard.index')}}">Dashboard</a></li>
+        <li><a href="{{route('dashboard.create')}}">Register</a></li>
     </ul>
 </nav>
